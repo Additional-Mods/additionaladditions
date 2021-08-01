7e090b889e5da87faa544180cb62bf678c48e810
Dqu1J <mcpeytwot5@gmail.com>
2021-08-02 01:32:21 +0300

    fix: Glow Sticks Replacing Blocks

--- src/main/java/dqu/additionaladditions/entity/GlowStickEntity.java
+++ src/main/java/dqu/additionaladditions/entity/GlowStickEntity.java
@@ -3,9 +3,11 @@ package dqu.additionaladditions.entity;
 import dqu.additionaladditions.AdditionalAdditions;
 import dqu.additionaladditions.Registrar;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
@@ -38,8 +40,14 @@ public class GlowStickEntity extends ThrownItemEntity {
         super.onCollision(hitResult);
         if (!this.world.isClient()) {
             this.remove(RemovalReason.DISCARDED);
-            BlockPos pos = new BlockPos(hitResult.getPos().getX(), hitResult.getPos().getY(), hitResult.getPos().getZ());
-            this.world.setBlockState(pos, Registrar.GLOW_STICK_BLOCK.getDefaultState());
+            BlockPos pos = new BlockPos(this.getX(), this.getY(), this.getZ());
+            if (this.world.getBlockState(pos).isAir()) {
+                this.world.setBlockState(pos, Registrar.GLOW_STICK_BLOCK.getDefaultState());
+            } else {
+                ItemStack stack = new ItemStack(Registrar.GLOW_STICK_ITEM, 1);
+                ItemEntity entity = new ItemEntity(this.world, this.getX(), this.getY(), this.getZ(), stack);
+                this.world.spawnEntity(entity);
+            }
         }
     }
 }
