f333523ce5d3aa9d70668b17ec72197af8e64ec7
Dqu1J <mcpeytwot5@gmail.com>
2021-08-23 13:26:52 +0300

    Watering Can: Now moisturizes farmlands

--- src/main/java/dqu/additionaladditions/item/WateringCanItem.java
+++ src/main/java/dqu/additionaladditions/item/WateringCanItem.java
@@ -8,6 +8,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -16,6 +17,7 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
@@ -36,17 +38,31 @@ public class WateringCanItem extends Item {
         if (hitResult.getType() != HitResult.Type.BLOCK) return TypedActionResult.fail(stack);
         BlockPos pos = hitResult.getBlockPos();
         BlockState state = world.getBlockState(pos);
+        BlockPos posBelow = pos.offset(Direction.DOWN);
+        BlockState stateBelow = world.getBlockState(posBelow);
 
-        if (state.getBlock() instanceof Fertilizable fertilizable && !(state.getBlock() instanceof GrassBlock)) {
-            if (stack.getDamage() > 0 || player.isCreative()) {
+        if (stack.getDamage() > 0 || player.isCreative()) {
+            if (state.getBlock() instanceof Fertilizable fertilizable && !(state.getBlock() instanceof GrassBlock)) {
                 player.playSound(SoundEvents.ITEM_BONE_MEAL_USE, 1.0F, 1.5F);
                 if (world.isClient()) return TypedActionResult.success(stack);
                 if (fertilizable.canGrow(world, world.random, pos, state)) {
                     if (world.random.nextFloat() < 0.25)
                         fertilizable.grow((ServerWorld) world, world.random, pos, state);
-                    stack.setDamage(stack.getDamage()-10);
-                    return TypedActionResult.success(stack);
                 }
+
+                if (stateBelow.getBlock() instanceof FarmlandBlock) {
+                    world.setBlockState(posBelow, stateBelow.with(Properties.MOISTURE, FarmlandBlock.MAX_MOISTURE));
+                }
+
+                stack.setDamage(stack.getDamage() - 10);
+                return TypedActionResult.success(stack);
+            }
+
+            if (state.getBlock() instanceof FarmlandBlock) {
+                if (world.isClient()) return TypedActionResult.success(stack);
+                world.setBlockState(pos, state.with(Properties.MOISTURE, FarmlandBlock.MAX_MOISTURE));
+                stack.setDamage(stack.getDamage() - 10);
+                return TypedActionResult.success(stack);
             }
         }
 
