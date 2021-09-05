6dba80d7398e5e4d798a7f4ef3cbd7c6e2e724fc
Dqu1J <mcpeytwot5@gmail.com>
2021-09-05 14:22:01 +0300

    fix: Offhand rendering while holding charged crossbow

--- src/main/java/dqu/additionaladditions/mixin/HeldItemRendererMixin.java
+++ src/main/java/dqu/additionaladditions/mixin/HeldItemRendererMixin.java
@@ -1,6 +1,7 @@
 package dqu.additionaladditions.mixin;
 
 import dqu.additionaladditions.registry.AdditionalItems;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
@@ -11,8 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HeldItemRenderer.class)
 public class HeldItemRendererMixin {
-    @Inject(method = "isChargedCrossbow", at = @At("HEAD"), cancellable = true)
-    private static void isCharged(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (stack.isOf(AdditionalItems.CROSSBOW_WITH_SPYGLASS) && CrossbowItem.isCharged(stack)) cir.setReturnValue(true);
+    @Inject(method = "getHandRenderType", at = @At("RETURN"), cancellable = true)
+    private static void isCharged(ClientPlayerEntity player, CallbackInfoReturnable<HeldItemRenderer.HandRenderType> cir) {
+        ItemStack stack = player.getMainHandStack();
+        if (stack.isOf(AdditionalItems.CROSSBOW_WITH_SPYGLASS) && CrossbowItem.isCharged(stack)) {
+            cir.setReturnValue(HeldItemRenderer.HandRenderType.RENDER_MAIN_HAND_ONLY);
+        }
     }
 }
