51ba61045896005321418c770316d68a8a0a455c
Dqu1J <mcpeytwot5@gmail.com>
2022-02-19 17:50:25 +0200

    more config stuff

--- src/main/java/dqu/additionaladditions/config/ConfigValues.java
+++ src/main/java/dqu/additionaladditions/config/ConfigValues.java
@@ -7,35 +7,35 @@ import dqu.additionaladditions.config.value.ListConfigValue;
 
 public enum ConfigValues {
     FOOD(1, new ConfigProperty("FoodItems", new ListConfigValue()
-            .put(new ConfigProperty("FriedEgg", new BooleanConfigValue(true)))
-            .put(new ConfigProperty("BerryPie", new BooleanConfigValue(true)))
-            .put(new ConfigProperty("HoneyedApple", new BooleanConfigValue(true)))
+            .put(new ConfigProperty("FriedEgg", ConfigValueType.TRUE))
+            .put(new ConfigProperty("BerryPie", ConfigValueType.TRUE))
+            .put(new ConfigProperty("HoneyedApple", ConfigValueType.TRUE))
     )),
-    WATERING_CAN(1, new ConfigProperty("WateringCan", new BooleanConfigValue(true))),
-    ROSE_GOLD( 1, new ConfigProperty("RoseGold", new BooleanConfigValue(true))),
-    ROPES(1, new ConfigProperty("Ropes", new BooleanConfigValue(true))),
-    ENCHANTMENT_PRECISION(1, new ConfigProperty("EnchantmentPrecision", new BooleanConfigValue(true))),
-    ENCHANTMENT_SPEED(3, new ConfigProperty("EnchantmentSpeed", new BooleanConfigValue(true))),
-    WRENCH(1, new ConfigProperty("Wrench", new BooleanConfigValue(true))),
-    COPPER_PATINA(1, new ConfigProperty("CopperPatina", new BooleanConfigValue(true))),
-    AMETHYST_LAMP(1, new ConfigProperty("AmethystLamp", new BooleanConfigValue(true))),
-    CROSSBOWS(1, new ConfigProperty("Crossbows", new BooleanConfigValue(true))),
-    TRIDENT_SHARD(2, new ConfigProperty("TridentShard", new BooleanConfigValue(true))),
-    GLOW_STICK(2, new ConfigProperty("GlowStick", new BooleanConfigValue(true))),
-    GILDED_NETHERITE(3, new ConfigProperty("GildedNetherite", new BooleanConfigValue(true))),
-    DEPTH_METER(3, new ConfigProperty("DepthMeter", new BooleanConfigValue(true))),
-    POTIONS(3, new ConfigProperty("Potions", new BooleanConfigValue(true))),
-    MYSTERIOUS_BUNDLE(3, new ConfigProperty("MysteriousBundle", new BooleanConfigValue(true))),
-    COMPOSTABLE_ROTTEN_FLESH(3, new ConfigProperty("CompostableRottenFlesh", new BooleanConfigValue(true))),
-    MUSIC_DISCS(4, new ConfigProperty("MusicDiscs", new BooleanConfigValue(true))),
-    NOTE_BLOCK_AMETHYST_SOUNDS(5, new ConfigProperty("NoteBlockAmethystSounds", new BooleanConfigValue(true))),
-    SHIPWRECK_SPYGLASS_LOOT(5, new ConfigProperty("ShipwreckSpyglassLoot", new BooleanConfigValue(true))),
-    POCKET_JUKEBOX(5, new ConfigProperty("PocketJukebox", new BooleanConfigValue(true))),
-    CHICKEN_NUGGET(5, new ConfigProperty("ChickenNugget", new BooleanConfigValue(true))),
-    POWERED_RAILS_COPPER_RECIPE(5, new ConfigProperty("PoweredRailsCopperRecipe", new BooleanConfigValue(true))),
-    GOLD_RING(5, new ConfigProperty("GoldRing", new BooleanConfigValue(true))),
+    WATERING_CAN(1, new ConfigProperty("WateringCan", ConfigValueType.TRUE)),
+    ROSE_GOLD( 1, new ConfigProperty("RoseGold", ConfigValueType.TRUE)),
+    ROPES(1, new ConfigProperty("Ropes", ConfigValueType.TRUE)),
+    ENCHANTMENT_PRECISION(1, new ConfigProperty("EnchantmentPrecision", ConfigValueType.TRUE)),
+    ENCHANTMENT_SPEED(3, new ConfigProperty("EnchantmentSpeed", ConfigValueType.TRUE)),
+    WRENCH(1, new ConfigProperty("Wrench", ConfigValueType.TRUE)),
+    COPPER_PATINA(1, new ConfigProperty("CopperPatina", ConfigValueType.TRUE)),
+    AMETHYST_LAMP(1, new ConfigProperty("AmethystLamp", ConfigValueType.TRUE)),
+    CROSSBOWS(1, new ConfigProperty("Crossbows", ConfigValueType.TRUE)),
+    TRIDENT_SHARD(2, new ConfigProperty("TridentShard", ConfigValueType.TRUE)),
+    GLOW_STICK(2, new ConfigProperty("GlowStick", ConfigValueType.TRUE)),
+    GILDED_NETHERITE(3, new ConfigProperty("GildedNetherite", ConfigValueType.TRUE)),
+    DEPTH_METER(3, new ConfigProperty("DepthMeter", ConfigValueType.TRUE)),
+    POTIONS(3, new ConfigProperty("Potions", ConfigValueType.TRUE)),
+    MYSTERIOUS_BUNDLE(3, new ConfigProperty("MysteriousBundle", ConfigValueType.TRUE)),
+    COMPOSTABLE_ROTTEN_FLESH(3, new ConfigProperty("CompostableRottenFlesh", ConfigValueType.TRUE)),
+    MUSIC_DISCS(4, new ConfigProperty("MusicDiscs", ConfigValueType.TRUE)),
+    NOTE_BLOCK_AMETHYST_SOUNDS(5, new ConfigProperty("NoteBlockAmethystSounds", ConfigValueType.TRUE)),
+    SHIPWRECK_SPYGLASS_LOOT(5, new ConfigProperty("ShipwreckSpyglassLoot", ConfigValueType.TRUE)),
+    POCKET_JUKEBOX(5, new ConfigProperty("PocketJukebox", ConfigValueType.TRUE)),
+    CHICKEN_NUGGET(5, new ConfigProperty("ChickenNugget", ConfigValueType.TRUE)),
+    POWERED_RAILS_COPPER_RECIPE(5, new ConfigProperty("PoweredRailsCopperRecipe", ConfigValueType.TRUE)),
+    GOLD_RING(5, new ConfigProperty("GoldRing", ConfigValueType.TRUE)),
     GLOW_BERRY_GLOW(6, new ConfigProperty("GlowBerryEatGlow", new ListConfigValue()
-            .put(new ConfigProperty("enabled", new BooleanConfigValue(true)))
+            .put(new ConfigProperty("enabled", ConfigValueType.TRUE))
             .put(new ConfigProperty("duration", new IntegerConfigValue(5)))
     ));
 
--- src/main/java/dqu/additionaladditions/config/value/ConfigValueType.java
+++ src/main/java/dqu/additionaladditions/config/value/ConfigValueType.java
@@ -4,5 +4,8 @@ public enum ConfigValueType {
     BOOLEAN,
     STRING,
     INTEGER,
-    LIST
+    LIST;
+
+    public static final BooleanConfigValue TRUE = new BooleanConfigValue(true);
+    public static final BooleanConfigValue FALSE = new BooleanConfigValue(false);
 }
