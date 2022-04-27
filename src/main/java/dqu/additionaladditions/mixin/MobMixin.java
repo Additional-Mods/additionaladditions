73c21b4826d37da8d5917c516d9866f5cd73d838
Dqu1J <mcpeytwot5@gmail.com>
2022-04-27 15:38:02 +0300

    Closes #32

--- src/main/java/dqu/additionaladditions/mixin/MobMixin.java
+++ src/main/java/dqu/additionaladditions/mixin/MobMixin.java
@@ -3,12 +3,12 @@ package dqu.additionaladditions.mixin;
 import dqu.additionaladditions.config.Config;
 import dqu.additionaladditions.config.ConfigValues;
 import dqu.additionaladditions.registry.AdditionalBlocks;
-import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.village.poi.PoiManager;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,18 +28,16 @@ public abstract class MobMixin extends LivingEntity {
     public void checkDespawn(CallbackInfo ci) {
         if (!isFeatureEnabled) return;
         if (this.tickCount > 0 || !shouldDespawnInPeaceful()) return;
-        if (isLampNearby((int)this.getX(), (int)this.getY(), (int)this.getZ(), 8)) {
+        PoiManager poiManager = ((ServerLevel)level).getPoiManager();
+        long count = poiManager.getCountInRange(
+                (poiType) -> poiType == AdditionalBlocks.AMETHYST_LAMP_POI,
+                blockPosition(),
+                8,
+                PoiManager.Occupancy.ANY
+        );
+
+        if (count > 0) {
             this.discard();
         }
-    }
-
-    public boolean isLampNearby(int x, int y, int z, int r) {
-        Iterable<BlockPos> poses = BlockPos.betweenClosed(x-r, y-r, z-r, x+r, y+r, z+r);
-        for (BlockPos pos : poses) {
-            if (level.getBlockState(pos).is(AdditionalBlocks.AMETHYST_LAMP)) {
-                if (level.getBlockState(pos).getValue(BlockStateProperties.LIT)) return true;
-            }
-        }
-        return false;
     }
 }
--- src/main/java/dqu/additionaladditions/registry/AdditionalBlocks.java
+++ src/main/java/dqu/additionaladditions/registry/AdditionalBlocks.java
@@ -1,5 +1,6 @@
 package dqu.additionaladditions.registry;
 
+import com.google.common.collect.ImmutableSet;
 import dqu.additionaladditions.AdditionalAdditions;
 import dqu.additionaladditions.block.CopperPatinaBlock;
 import dqu.additionaladditions.block.GlowStickBlock;
@@ -8,12 +9,15 @@ import dqu.additionaladditions.block.RopeBlock;
 import dqu.additionaladditions.item.CopperPatinaItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.RedstoneLampBlock;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Material;
 
 public class AdditionalBlocks {
@@ -22,6 +26,8 @@ public class AdditionalBlocks {
     public static final CopperPatinaBlock COPPER_PATINA = new CopperPatinaBlock(FabricBlockSettings.of(Material.CLOTH_DECORATION).noCollission().sound(SoundType.TUFF).instabreak());
     public static final GlowStickBlock GLOW_STICK_BLOCK = new GlowStickBlock(FabricBlockSettings.of(Material.CLOTH_DECORATION).noCollission().lightLevel((state) -> 12).instabreak());
     public static final PatinaBlock PATINA_BLOCK = new PatinaBlock(FabricBlockSettings.of(Material.SAND).sound(SoundType.ROOTED_DIRT).strength(0.5f));
+
+    public static PoiType AMETHYST_LAMP_POI;
 
     public static void registerAll() {
         Registry.register(Registry.BLOCK, new ResourceLocation(AdditionalAdditions.namespace, "glow_stick"), GLOW_STICK_BLOCK);
@@ -37,5 +43,12 @@ public class AdditionalBlocks {
         Registry.register(Registry.BLOCK, new ResourceLocation(AdditionalAdditions.namespace, "patina_block"), PATINA_BLOCK);
         Registry.register(Registry.ITEM, new ResourceLocation(AdditionalAdditions.namespace, "patina_block"),
                 new BlockItem(PATINA_BLOCK, new FabricItemSettings().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+
+        AMETHYST_LAMP_POI = PointOfInterestHelper.register(
+            new ResourceLocation(AdditionalAdditions.namespace, "amethyst_lamp_poi"),
+            0, 8,
+            ImmutableSet.copyOf(
+                    AdditionalBlocks.AMETHYST_LAMP.getStateDefinition().getPossibleStates().stream().filter(state -> state.getValue(BlockStateProperties.LIT)).toList()
+        ));
     }
 }
