d646e2d517a726934e38e2df1ac6c75997c2d4d7
Dqu1J <mcpeytwot5@gmail.com>
2022-04-28 20:55:01 +0300

    more haste potion recipes

--- src/main/java/dqu/additionaladditions/mixin/PotionBrewingMixin.java
+++ src/main/java/dqu/additionaladditions/mixin/PotionBrewingMixin.java
@@ -29,5 +29,7 @@ public abstract class PotionBrewingMixin {
         addMix(Potions.SWIFTNESS, Items.AMETHYST_SHARD, AdditionalPotions.HASTE_POTION);
         addMix(AdditionalPotions.HASTE_POTION, Items.REDSTONE, AdditionalPotions.LONG_HASTE_POTION);
         addMix(AdditionalPotions.HASTE_POTION, Items.GLOWSTONE_DUST, AdditionalPotions.STRONG_HASTE_POTION);
+        addMix(Potions.STRONG_SWIFTNESS, Items.AMETHYST_SHARD, AdditionalPotions.STRONG_HASTE_POTION);
+        addMix(Potions.LONG_SWIFTNESS, Items.AMETHYST_SHARD, AdditionalPotions.LONG_HASTE_POTION);
     }
 }
