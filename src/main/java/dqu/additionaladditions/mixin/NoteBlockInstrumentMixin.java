63d42ee21e8b9fcb9912929d9256d839b944a714
foul-fortune-feline <foul-fortune-feline@pm.me>
2022-05-02 13:49:12 -0700

    Disabled modmenu support, as libGUI is broken in 22w17a :<
    For now, people will have to make use of the config files
    
    If someone else wants to update libgui or cut out the parts that don't depend on it and reimplement them, I'll leave that up to y'all. I've other projects to attend however.

--- src/main/java/dqu/additionaladditions/mixin/NoteBlockInstrumentMixin.java
+++ src/main/java/dqu/additionaladditions/mixin/NoteBlockInstrumentMixin.java
@@ -47,32 +47,4 @@ public class NoteBlockInstrumentMixin {
             ci.setReturnValue(AMETHYST);
         }
     }
-
-//    @SuppressWarnings("InvokerTarget")
-//    @Invoker("<init>")
-//    private static NoteBlockInstrument newInstrument(String internalName, int internalId, String name, SoundEvent sound) {
-//        throw new AssertionError();
-//    }
-//
-//    @SuppressWarnings("ShadowTarget")
-//    @Shadow
-//    @Mutable
-//    private static @Final NoteBlockInstrument[] field_12652;
-//
-//    @SuppressWarnings("UnresolvedMixinReference")
-//    @Inject(method = "<clinit>", at = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/world/level/block/state/properties/NoteBlockInstrument;field_12652:[Lnet/minecraft/world/level/block/state/properties/NoteBlockInstrument;", shift = At.Shift.AFTER))
-//    private static void addCustomInstrument(CallbackInfo ci) {
-//        var variants = new ArrayList<>(Arrays.asList(field_12652));
-//        var last = variants.get(variants.size() - 1);
-//        var amethyst = newInstrument("AMETHYST", last.ordinal()+1, "amethyst", SoundEvents.AMETHYST_CLUSTER_PLACE);
-//        AdditionalInstrument.AMETHYST = amethyst;
-//        variants.add(amethyst);
-//        field_12652 = variants.toArray(new NoteBlockInstrument[0]);
-//    }
-//
-//    @Inject(method = "byState", at = @At("RETURN"), cancellable = true)
-//    private static void amethystInstrument(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> cir) {
-//        if (state.is(Blocks.AMETHYST_BLOCK) && Config.getBool(ConfigValues.NOTE_BLOCK_AMETHYST_SOUNDS))
-//            cir.setReturnValue(AdditionalInstrument.AMETHYST);
-//    }
 }
