91254971477c8707879492f5ea9f16655b5ed8de
Dqu1J <mcpeytwot5@gmail.com>
2022-05-17 20:57:44 +0300

    Fixes #52
    Thanks @foul-fortune-feline

--- src/main/java/dqu/additionaladditions/enchantment/SpeedEnchantment.java
+++ src/main/java/dqu/additionaladditions/enchantment/SpeedEnchantment.java
@@ -19,19 +19,19 @@ public class SpeedEnchantment extends Enchantment {
 
     @Override
     protected boolean checkCompatibility(Enchantment other) {
-        if (Config.getBool(ConfigValues.ENCHANTMENT_SPEED)) return false;
-        if (other == Enchantments.SOUL_SPEED || other == Enchantments.FROST_WALKER || other == Enchantments.DEPTH_STRIDER)
+        if (other == Enchantments.SOUL_SPEED || other == Enchantments.FROST_WALKER || other == Enchantments.DEPTH_STRIDER) {
             return false;
+        }
         return super.checkCompatibility(other);
     }
 
     @Override
     public boolean isDiscoverable() {
-        return Config.getBool(ConfigValues.ENCHANTMENT_PRECISION);
+        return Config.getBool(ConfigValues.ENCHANTMENT_SPEED);
     }
 
     @Override
     public boolean isTradeable() {
-        return Config.getBool(ConfigValues.ENCHANTMENT_PRECISION);
+        return Config.getBool(ConfigValues.ENCHANTMENT_SPEED);
     }
 }
