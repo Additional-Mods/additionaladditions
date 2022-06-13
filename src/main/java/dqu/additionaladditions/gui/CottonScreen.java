b7dbe1b47180ceda2c9116e12a2f2a17037f551f
Dqu1J <mcpeytwot5@gmail.com>
2022-06-14 01:37:26 +0300

    fix libgui, lambdynamiclights, ingame config; bump modmenu

--- src/main/java/dqu/additionaladditions/gui/CottonScreen.java
+++ src/main/java/dqu/additionaladditions/gui/CottonScreen.java
@@ -1,10 +1,10 @@
-//package dqu.additionaladditions.gui;
-//
-//import io.github.cottonmc.cotton.gui.GuiDescription;
-//import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
-//
-//public class CottonScreen extends CottonClientScreen {
-//    public CottonScreen(GuiDescription description) {
-//        super(description);
-//    }
-//}
+package dqu.additionaladditions.gui;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class CottonScreen extends CottonClientScreen {
+    public CottonScreen(GuiDescription description) {
+        super(description);
+    }
+}
