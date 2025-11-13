03b6c7d69f7f490e694e98744690cb50f2659904
Dqu1J <mcpeytwot5@gmail.com>
2025-11-13 16:38:40 +0000

    add: rose gold block

--- src/main/java/dqu/additionaladditions/registry/AdditionalBlocks.java
+++ src/main/java/dqu/additionaladditions/registry/AdditionalBlocks.java
@@ -18,6 +18,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RedstoneLampBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -38,6 +39,7 @@ public class AdditionalBlocks {
     public static final CopperPatinaBlock COPPER_PATINA = new CopperPatinaBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_CYAN).pushReaction(PushReaction.DESTROY).noCollission().sound(SoundType.TUFF).instabreak());
     public static final GlowStickBlock GLOW_STICK_BLOCK = new GlowStickBlock(BlockBehaviour.Properties.of().mapColor(MapColor.NONE).pushReaction(PushReaction.DESTROY).noCollission().lightLevel((state) -> 12).instabreak());
     public static final PatinaBlock PATINA_BLOCK = new PatinaBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_CYAN).pushReaction(PushReaction.NORMAL).sound(SoundType.ROOTED_DIRT).strength(0.5f));
+    public static final Block ROSE_GOLD_BLOCK = new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PINK).requiresCorrectToolForDrops().strength(3.0f, 6.0f).sound(SoundType.METAL));
 
     public static PoiType AMETHYST_LAMP_POI;
     public static ResourceLocation AMETHYST_LAMP_POI_RL = ResourceLocation.tryBuild(AdditionalAdditions.namespace, "amethyst_lamp_poi");
@@ -61,6 +63,10 @@ public class AdditionalBlocks {
         Registry.register(BuiltInRegistries.ITEM, ResourceLocation.tryBuild(AdditionalAdditions.namespace, "patina_block"),
                 new BlockItem(PATINA_BLOCK, new Item.Properties()));
 
+        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.tryBuild(AdditionalAdditions.namespace, "rose_gold_block"), ROSE_GOLD_BLOCK);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.tryBuild(AdditionalAdditions.namespace, "rose_gold_block"),
+                new BlockItem(ROSE_GOLD_BLOCK, new Item.Properties()));
+
         AMETHYST_LAMP_POI = PointOfInterestHelper.register(
             AMETHYST_LAMP_POI_RL,
             0, 8,
@@ -77,8 +83,9 @@ public class AdditionalBlocks {
         CreativeAdder.REDSTONE_BLOCKS.add(() -> Config.getBool(ConfigValues.COPPER_PATINA), Items.REDSTONE_BLOCK, COPPER_PATINA);
         CreativeAdder.REDSTONE_BLOCKS.add(() -> Config.getBool(ConfigValues.AMETHYST_LAMP, "enabled"), Items.REDSTONE_LAMP, AMETHYST_LAMP);
         CreativeAdder.FUNCTIONAL_BLOCKS.add(() -> Config.getBool(ConfigValues.AMETHYST_LAMP, "enabled"), Items.REDSTONE_LAMP, AMETHYST_LAMP);
+        CreativeAdder.FUNCTIONAL_BLOCKS.add(() -> Config.getBool(ConfigValues.ROPES), Items.LADDER, ROPE_BLOCK);
         CreativeAdder.BUILDING_BLOCKS.add(() -> Config.getBool(ConfigValues.COPPER_PATINA), Items.REDSTONE_BLOCK, PATINA_BLOCK);
-        CreativeAdder.BUILDING_BLOCKS.add(() -> Config.getBool(ConfigValues.ROPES), Items.CHAIN, ROPE_BLOCK);
+        CreativeAdder.BUILDING_BLOCKS.add(() -> Config.getBool(ConfigValues.ROSE_GOLD), Items.LIGHT_WEIGHTED_PRESSURE_PLATE, ROSE_GOLD_BLOCK);
         CreativeAdder.TOOLS_AND_UTILITIES.add(() -> Config.getBool(ConfigValues.ROPES), Items.SPYGLASS, ROPE_BLOCK);
     }
 }
