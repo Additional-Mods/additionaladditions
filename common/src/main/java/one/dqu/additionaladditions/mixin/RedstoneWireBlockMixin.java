17d2e15d4acfceaf00d5ff87d2a345d85ddbbc1c
Dqu1J <mcpeytwot5@gmail.com>
2025-11-23 12:59:06 +0000

    refactor: rewrite copper patina

--- common/src/main/java/one/dqu/additionaladditions/mixin/RedstoneWireBlockMixin.java
+++ common/src/main/java/one/dqu/additionaladditions/mixin/RedstoneWireBlockMixin.java
@@ -1,42 +1,106 @@
 package one.dqu.additionaladditions.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.properties.RedstoneSide;
 import one.dqu.additionaladditions.registry.AdditionalBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * Prevents redstone dust from connecting to copper patina and from receiving power through it.
+ * Prevents redstone wire and copper patina from connecting to each other.
  */
 @Mixin(RedStoneWireBlock.class)
-public class RedstoneWireBlockMixin {
-    @Inject(method = "shouldConnectTo(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;)Z", at = @At("RETURN"), cancellable = true)
-    private static void connectsTo(BlockState state, Direction dir, CallbackInfoReturnable<Boolean> callbackInfo) {
-        if (state.is(AdditionalBlocks.COPPER_PATINA.get())) {
-            callbackInfo.setReturnValue(false);
+public abstract class RedstoneWireBlockMixin {
+    @Shadow
+    protected abstract boolean canSurviveOn(BlockGetter blockGetter, BlockPos blockPos, BlockState blockState);
+
+    @Inject(
+            method = "getConnectingSide(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;Z)Lnet/minecraft/world/level/block/state/properties/RedstoneSide;",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    protected void getConnectingSide(BlockGetter blockGetter, BlockPos blockPos, Direction direction, boolean bl, CallbackInfoReturnable<RedstoneSide> cir) {
+        Block self = (Block) (Object) this;
+        boolean isCopperPatina = self == AdditionalBlocks.COPPER_PATINA.get();
+        boolean isRedstoneWire = self == Blocks.REDSTONE_WIRE;
+        if (!isCopperPatina && !isRedstoneWire) {
+            return;
+        }
+        cir.setReturnValue(
+                additionaladditions$getConnectingSide(blockGetter, blockPos, direction, bl, isCopperPatina)
+        );
+    }
+
+    @Unique
+    private RedstoneSide additionaladditions$getConnectingSide(BlockGetter blockGetter, BlockPos blockPos, Direction direction, boolean bl, boolean isCopperPatina) {
+        BlockPos blockPos2 = blockPos.relative(direction);
+        BlockState blockState = blockGetter.getBlockState(blockPos2);
+        if (bl) {
+            boolean bl2 = blockState.getBlock() instanceof TrapDoorBlock || this.canSurviveOn(blockGetter, blockPos2, blockState);
+            if (bl2 && additionaladditions$shouldConnectTo(blockGetter.getBlockState(blockPos2.above()), null, isCopperPatina)) {
+                if (blockState.isFaceSturdy(blockGetter, blockPos2, direction.getOpposite())) {
+                    return RedstoneSide.UP;
+                }
+
+                return RedstoneSide.SIDE;
+            }
+        }
+
+        return !additionaladditions$shouldConnectTo(blockState, direction, isCopperPatina) && (blockState.isRedstoneConductor(blockGetter, blockPos2) || !additionaladditions$shouldConnectTo(blockGetter.getBlockState(blockPos2.below()), null, isCopperPatina)) ? RedstoneSide.NONE : RedstoneSide.SIDE;
+    }
+
+    @Unique
+    private static boolean additionaladditions$shouldConnectTo(BlockState blockState, @Nullable Direction direction, boolean isCopperPatina) {
+        if (blockState.is(AdditionalBlocks.COPPER_PATINA.get())) {
+            return isCopperPatina;
+        } else if (blockState.is(Blocks.REDSTONE_WIRE)) {
+            return !isCopperPatina;
+        } else if (blockState.is(Blocks.REPEATER)) {
+            Direction direction2 = blockState.getValue(RepeaterBlock.FACING);
+            return direction2 == direction || direction2.getOpposite() == direction;
+        } else if (blockState.is(Blocks.OBSERVER)) {
+            return direction == blockState.getValue(ObserverBlock.FACING);
+        } else {
+            return blockState.isSignalSource() && direction != null;
         }
     }
 
-    @Redirect(method = "calculateTargetStrength", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBestNeighborSignal(Lnet/minecraft/core/BlockPos;)I"))
-    private int getReplacedReceivedRedstonePower(Level world, BlockPos pos) {
-        int i = 0;
-        Direction[] var3 = Direction.values();
-        int var4 = var3.length;
+    @WrapOperation(
+            method = "calculateTargetStrength",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;getBestNeighborSignal(Lnet/minecraft/core/BlockPos;)I"
+            )
+    )
+    private static int getBestNeighborSignal(Level level, BlockPos blockPos, Operation<Integer> original) {
+        BlockState blockState = level.getBlockState(blockPos);
+        boolean isCopperPatina = blockState.is(AdditionalBlocks.COPPER_PATINA.get());
+        boolean isRedstoneWire = blockState.is(Blocks.REDSTONE_WIRE);
 
-        for(int var5 = 0; var5 < var4; ++var5) {
-            Direction direction = var3[var5];
-            BlockPos offsetted = pos.relative(direction);
-            BlockState state = world.getBlockState(offsetted);
+        if (!isCopperPatina && !isRedstoneWire) {
+            return original.call(level, blockPos);
+        }
+
+        int i = 0;
+        for (Direction direction : Direction.values()) {
+            BlockState offsetState = level.getBlockState(blockPos.relative(direction));
+            boolean flag = isCopperPatina ? !offsetState.is(Blocks.REDSTONE_WIRE) : !offsetState.is(AdditionalBlocks.COPPER_PATINA.get());
+
             int j = 0;
-            if (!state.is(AdditionalBlocks.COPPER_PATINA.get())) {
-                j = world.getSignal(offsetted, direction);
+            if (flag) {
+                j = level.getSignal(blockPos.relative(direction), direction);
             }
             if (j >= 15) {
                 return 15;
@@ -45,7 +109,6 @@ public class RedstoneWireBlockMixin {
                 i = j;
             }
         }
-
         return i;
     }
 }
