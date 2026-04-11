634f757c14926003e8c3a235a93c903716c8a198
Dqu1J <mcpeytwot5@gmail.com>
2026-04-11 18:41:50 +0100

    1.21.11: neoforge: move to new transaction api

--- neoforge/src/main/java/one/dqu/additionaladditions/util/neoforge/FluidHelperImpl.java
+++ neoforge/src/main/java/one/dqu/additionaladditions/util/neoforge/FluidHelperImpl.java
@@ -2,14 +2,14 @@ package one.dqu.additionaladditions.util.neoforge;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.capabilities.Capabilities;
-import net.neoforged.neoforge.fluids.FluidStack;
-import net.neoforged.neoforge.fluids.capability.IFluidHandler;
-import net.neoforged.neoforge.transfer.fluid.FluidResource;
 import net.neoforged.neoforge.transfer.ResourceHandler;
+import net.neoforged.neoforge.transfer.fluid.FluidResource;
+import net.neoforged.neoforge.transfer.transaction.Transaction;
 import one.dqu.additionaladditions.util.FluidHelper;
 
 public class FluidHelperImpl {
@@ -20,15 +20,20 @@ public class FluidHelperImpl {
         if (handler == null) {
             return new FluidHelper.Transaction(0, (committed) -> {});
         }
-        IFluidHandler capability = IFluidHandler.of(handler);
 
-        FluidStack fluidStack = new FluidStack(fluid, amount);
-        FluidStack drained = capability.drain(fluidStack, IFluidHandler.FluidAction.SIMULATE);
+        FluidResource resource = FluidResource.of(fluid);
 
-        return new FluidHelper.Transaction(drained.getAmount(), (committed) -> {
-            FluidStack toDrain = new FluidStack(fluid, committed);
-            toDrain.getFluid().getPickupSound().ifPresent((sound) -> level.playSound(null, pos, sound, net.minecraft.sounds.SoundSource.BLOCKS));
-            capability.drain(toDrain, IFluidHandler.FluidAction.EXECUTE);
+        int available;
+        try (var tx = Transaction.openRoot()) {
+            available = handler.extract(resource, amount, tx);
+        }
+
+        return new FluidHelper.Transaction(available, (committed) -> {
+            try (var tx = Transaction.openRoot()) {
+                handler.extract(resource, committed, tx);
+                tx.commit();
+            }
+            fluid.getPickupSound().ifPresent((sound) -> level.playSound(null, pos, sound, SoundSource.BLOCKS));
         });
     }
 }
