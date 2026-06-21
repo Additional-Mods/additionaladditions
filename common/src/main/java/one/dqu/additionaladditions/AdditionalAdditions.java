93363c852e717553b0dbeedf28898f1a64f0d8dd
Dqu1J <mcpeytwot5@gmail.com>
2026-06-21 12:53:54 +0100

    26.1: cleanup todo list

--- common/src/main/java/one/dqu/additionaladditions/AdditionalAdditions.java
+++ common/src/main/java/one/dqu/additionaladditions/AdditionalAdditions.java
@@ -7,15 +7,6 @@ import one.dqu.additionaladditions.registry.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
-TODOs:
-- make sure datagen generates stuff properly
-- make sure All old manual data that datagen handles is removed
-- maybe loottable datagen
-- maybe furnace/cooking recipes datagen
-
-- test for any remaining issues on 26.1 port
- */
 public final class AdditionalAdditions {
     public static final String NAMESPACE = "additionaladditions";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
