20b5447c8b297b7b478bbbdc9621bb909bfede1e
Dqu1J <mcpeytwot5@gmail.com>
2026-06-21 12:50:46 +0100

    26.1: readd REI compat

---
+++ neoforge/src/main/java/one/dqu/additionaladditions/recipe/neoforge/REINeoforgeCommonCompat.java
@@ -0,0 +1,8 @@
+package one.dqu.additionaladditions.recipe.neoforge;
+
+import me.shedaniel.rei.forge.REIPluginCommon;
+import one.dqu.additionaladditions.recipe.REICommonCompat;
+
+@REIPluginCommon
+public class REINeoforgeCommonCompat extends REICommonCompat {
+}
